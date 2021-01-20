054405285e6158978f3633006af7662d0907c53f
janik6882 <janik@klauenberg.eu>
2021-01-20 15:32:18 +0100

    backend von main.py zu backend.py verschoben
    zur besseren übersicht und kompatibilität mit den anderen branches wurde der backend inhalt von main.py in backend.py verschoben

---
+++ backend.py
@@ -0,0 +1,6 @@
+class game():
+    def __init__(self, nodes, boardY, boardX):
+        self.nodes = nodes
+        self.boardX = boardX
+        self.boardY = boardY
+    
--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-class game():
-    def __init__(self, nodes, boardY, boardX):
-        self.nodes = nodes
-        self.boardX = boardX
-        self.boardY = boardY
+
