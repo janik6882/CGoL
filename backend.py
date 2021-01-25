4915564216297a11e670503d59cbc6eb6ddb130e
janik6882 <janik@klauenberg.eu>
2021-01-25 11:58:06 +0100

    get_point, add_point, remove_point, manipulate_point hinzugefuegt

--- backend.py
+++ backend.py
@@ -4,3 +4,63 @@ class game():
         self.nodes = nodes
         self.boardX = boardX
         self.boardY = boardY
+
+    def get_points(self):
+        """
+        Kommentar: gibt die aktuelle knotenliste aus
+        Input: Name der Instanz
+        Output: Knotenliste self.node
+        Besonders: Rückgabe mittels return
+        """
+        return self.nodes
+
+    def add_point(self, x, y):
+        """
+        Kommentar: Fuegt einen Punkt zur knotenliste self.nodes hinzu
+        Input: name der instanze, x-koordinate als int, y-koordinate als int
+        Output: Aktualisierte knotenliste self.nodes
+        Besonders: Weitere Optimierung der Laufzeit
+        """
+        # TODO: Verhalten besprechen, wenn punkt bereits in Knotenliste
+        # OPTIMIZE: Laufzeit optimieren (add, append oder operator?)
+        self.nodes.append([x, y])
+        return self.nodes
+
+    def remove_point(self, x, y):
+        """
+        Kommentar: Entfernt einen Punkt aus der Knotenliste
+        Input: Name der Instanz, x-koordinate als int, y-koordinate als int
+        Output: Aktualisierte Knotenliste self.nodes
+        Besonders: Falls Punkt nicht in Knotenliste erfolgt Error
+        """
+        # OPTIMIZE: Laufzeit?
+        # DEBUG: Try except pruefen
+        if [x, y] in self.nodes:
+            self.nodes.remove([x, y])
+            return self.nodes
+        else:
+            # Ausgabe besprechen
+            return "error"
+
+    def manipulate_point(self, x, y):
+        """
+        Kommentar: Fuegt Punkt hinzu, wenn nicht vorhanden, entfernt wenn vorhanden
+        Input: Name der Instanz, x-koordinate als int, y-koordinate als int
+        Output: Aktualisierte Knotenliste
+        Besonders: Prueft ob Punkt in Knotenliste
+        """
+        if [x, y] in self.nodes:
+            out = self.remove_point(x, y)
+        else:
+            out = self.add_point(x, y)
+        return out
+
+
+def debug():
+    test = game()
+    print (test.get_points())
+    print (test.add_point(1, 1))
+    print (test.remove_point(1,1))
+
+if __name__ == '__main__':
+    debug()
