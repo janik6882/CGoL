125a853c022b3ce283420e491fcb18c6dc11595c
janik6882 <janik@klauenberg.eu>
2021-02-17 18:44:58 +0100

    multiprocessing funktioniert, weitere integration nötig

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Main Datei für das Projekt."""
-from frontend import Display
-import subprocess
 import multiprocessing
+import subprocess
+from frontend import Display
 
 
 def menu_proc():
@@ -10,15 +10,18 @@ def menu_proc():
 
 
 def main():
+    """Doctstring."""  # TODO: docu beenden
     glider_top_left = [[1, 2], [1, 3], [1, 4], [1, 5], [2, 1], [2, 5], [3, 5],
                        [4, 1], [4, 4]]
+    proc = multiprocessing.Process(target=menu_proc)
+    proc.start()
+    # proc.join()
     test = Display(1000, 1000, glider_top_left)
     print(test.game.list_premade())
     test.game.add_premade("Middle-weight spaceship", 5, 5)
-    p = multiprocessing.Process(target=menu_proc())
-    p.start()
-    # p.join()
     test.mainloop()
 
+
 # Programm wird aus unerklärlichen Gründen doppelt ausgeführt.
-main()
+if __name__ == "__main__":
+    main()
