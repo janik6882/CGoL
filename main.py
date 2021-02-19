c295f97f28aa98f78c7de21a3dcd43233c1da462
janik6882 <janik@klauenberg.eu>
2021-02-19 20:51:20 +0100

    kommunikation menu-main bearbeitet

--- main.py
+++ main.py
@@ -2,31 +2,30 @@
 import multiprocessing
 import subprocess
 import json
-from frontend import Display
-import pygame
 import ast
+from frontend import Display
+# import pygame
 # queue = multiprocessing.Queue()
-res_data = dict()
 
 # manager = multiprocessing.Manager()
 # return_dict = manager.dict()
 
 def menu_proc():
-    global res_data
-    """Doctstring."""  # TODO: add docstring
+    """Öffnet den GUI.py prozess.
+
+    Einstellungen werden in menu.json gespeichert
+    """
     res = subprocess.check_output("python GUI.py")
     # global return_dict
     data = res.decode("utf-8")
     data = ast.literal_eval(data)
+    res_data = dict()
     res_data["res"] = data
     json.dump(res_data, open("menu.json", "w"))
-    # return_dict["res"] = data
-    # print(res.decode("utf-8"))
-    # print(res)
 
 
 def open_menu():
-    global res_data
+    global choice
     """Ruft die menu_proc() methode mit Multiprocessing auf.
 
     Kommentar: Öffnet das menü über Multiprocessing
@@ -40,16 +39,16 @@ def open_menu():
     proc = multiprocessing.Process(target=menu_proc)
     jobs.append(proc)
     proc.start()
-    print(res_data)
-    return("lol")
+    choice = json.load(open("menu.json"))
     # print(return_dict.values())
 
     # command = proc.get()
     # return command
 
+
 def main():
     """Doctstring."""  # TODO: docu beenden
-    global res_data
+    global choice
     glider_top_left = [[1, 2], [1, 3], [1, 4], [1, 5], [2, 1], [2, 5], [3, 5],
     [4, 1], [4, 4]]
     # proc.join()
@@ -66,7 +65,7 @@ def main():
             choice = json.load(open("menu.json", "r"))
             print(choice)
         test.game.next_board()
-        print(res_data)
+        print(x)
         print("test")
         Display.check_close()
     # test.mainloop()
