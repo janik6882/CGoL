a69ff8657313aba9977d7a3f2cfe8b093dfbf470
janik6882 <janik@klauenberg.eu>
2021-03-05 12:01:42 +0100

    kleinere änderungen, open_menu nun über hotkey m aufrufbar

--- frontend.py
+++ frontend.py
@@ -17,7 +17,7 @@ class Display():  # Zu Display ändern
     backend.py importiertem game.
     """
 
-    def __init__(self, master, windowX: int, windowY: int, nodes=None):
+    def __init__(self, windowX: int, windowY: int, nodes=None):
         """Init Klasse.
 
         Kommentar: Konstrukter der Klasse display()
@@ -27,46 +27,6 @@ class Display():  # Zu Display ändern
         Besonders: Erstellt ein pygame display, erstellt eine Instanz der
                    game() klasse
         """
-
-        self.master = master
-        master.geometry("250x250")
-
-        master.title("Conways Game of Life")
-
-        self.label = Label(master, text="Start Menu")
-        self.label.grid(row=0, column=0, columnspan=2, sticky='ew')
-
-        self.play_button = Button(master, text="Play")
-        self.play_button.grid(row=1, column=0, sticky='ew')
-
-        self.pause_button = Button(master, text="Pause")
-        self.pause_button.grid(row=2, column=0, sticky='ew')
-
-        self.save_button = Button(master, text="Save", command=lambda: Display.save_file())
-        self.save_button.grid(row=3, column=0, sticky='ew')
-
-        self.load_button = Button(master, text="Load", command=lambda: Display.open_file())
-        self.load_button.grid(row=4, column=0, sticky='ew')
-
-        self.rules_button = Button(master, text="Rules")
-        self.rules_button.grid(row=1, column=1, sticky='ew')
-
-        self.forms_button = Button(master, text="Forms")
-        self.forms_button.grid(row=2, column=1, sticky='ew')
-
-        self.manual_button = Button(master, text="Manual")
-        self.manual_button.grid(row=3, column=1, sticky='ew')
-
-        self.auto_button = Button(master, text="Auto")
-        self.auto_button.grid(row=4, column=1, sticky='ew')
-
-        self.master.columnconfigure(0, weight=1, uniform="commi")
-        self.master.columnconfigure(1, weight=1, uniform="commi")
-        self.master.rowconfigure(1, weight=1, uniform="commi")
-        self.master.rowconfigure(2, weight=1, uniform="commi")
-        self.master.rowconfigure(3, weight=1, uniform="commi")
-        self.master.rowconfigure(4, weight=1, uniform="commi")
-
         nodes = nodes or []
         self.window_x = windowX
         self.window_y = windowY
@@ -88,6 +48,48 @@ class Display():  # Zu Display ändern
         """
         self.display.fill(self.white)
         self.draw_grid()
+
+    def open_menu(self):
+        # TODO: doku hinzufuegen
+        self.master = Tk()
+        self.master.geometry("250x250")
+
+        self.master.title("Conways Game of Life")
+
+        self.label = Label(self.master, text="Start Menu")
+        self.label.grid(row=0, column=0, columnspan=2, sticky='ew')
+
+        self.play_button = Button(self.master, text="Play")
+        self.play_button.grid(row=1, column=0, sticky='ew')
+
+        self.pause_button = Button(self.master, text="Pause")
+        self.pause_button.grid(row=2, column=0, sticky='ew')
+
+        self.save_button = Button(self.master, text="Save", command=lambda: Display.save_file())
+        self.save_button.grid(row=3, column=0, sticky='ew')
+
+        self.load_button = Button(self.master, text="Load", command=lambda: Display.open_file())
+        self.load_button.grid(row=4, column=0, sticky='ew')
+
+        self.rules_button = Button(self.master, text="Rules")
+        self.rules_button.grid(row=1, column=1, sticky='ew')
+
+        self.forms_button = Button(self.master, text="Forms")
+        self.forms_button.grid(row=2, column=1, sticky='ew')
+
+        self.manual_button = Button(self.master, text="Manual")
+        self.manual_button.grid(row=3, column=1, sticky='ew')
+
+        self.auto_button = Button(self.master, text="Auto")
+        self.auto_button.grid(row=4, column=1, sticky='ew')
+
+        self.master.columnconfigure(0, weight=1, uniform="commi")
+        self.master.columnconfigure(1, weight=1, uniform="commi")
+        self.master.rowconfigure(1, weight=1, uniform="commi")
+        self.master.rowconfigure(2, weight=1, uniform="commi")
+        self.master.rowconfigure(3, weight=1, uniform="commi")
+        self.master.rowconfigure(4, weight=1, uniform="commi")
+        self.master.mainloop()
 
     def draw_grid(self):
         """Zeichnet ein Gitter.
@@ -177,6 +179,8 @@ class Display():  # Zu Display ändern
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         sys.exit()
+                    if event.key == pygame.K_m:
+                        self.open_menu()
 
     def mainloop(self):
         """Mainloop, läuft bis beendet.
@@ -265,10 +269,9 @@ def main():
 
 def debug():
     """Funktion zum Debugging."""
-    root = Tk()
-    fenster = Display(root, 0,0)
-    root.mainloop()
 
+    fenster = Display(0,0)
+    fenster.mainloop()
 
 if __name__ == '__main__':
     debug()
